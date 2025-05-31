7e5932c34124076c277e4418bd1c40b028c96a32
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-05-31 18:13:29 +0530

    Using toJoinString() method to print array

--- src/Arrays/Basic 1.kt
+++ src/Arrays/Basic 1.kt
@@ -9,6 +9,16 @@ fun main() {
     val greeting = arrayOf<String>("Hello", "Bhailog", ",", "Kese", "Ho", "?") // explicit type declaration
     // means this array is of only Int Data Types
 
+    // Printing all elements of Array
+    // This will not work properly
+    print("Here is the Address for Array Collection : ")
+    println(marks)
+
+    // Whereas this will because array is collection framework and will returns address
+    // And By using joinToString() method , we can have all values printed in sequential
+    // manner seperated by , (comma)
+    println(marks.joinToString())
+
     // Transverse Each Element of an Array
     // Using Size
     println("\n\nElements in Array(Marks) are : ")
